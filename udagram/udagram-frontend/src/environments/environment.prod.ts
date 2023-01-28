5d60523d61591178019c9100a6c4cd2d06b75396
Mohab2000 <mohabahmed20005@gmail.com>
2023-01-28 19:00:40 +0200

    all conifgurations done

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,9 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost:
+    "http://udagrambackend-env.eba-t9z2rdg7.us-east-1.elasticbeanstalk.com",
+  //"http://localhost:8080/api/v0",
 };
 
 /*
