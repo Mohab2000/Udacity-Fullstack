6324eb5db1d2f0b2f2649949657cee60d174ab34
Mohab2000 <mohabahmed20005@gmail.com>
2023-01-29 15:03:23 +0200

    configurations

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,16 +1,16 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=Mohab
-export POSTGRES_HOST=database-1.cojokxty5mbd.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=udagram-1.cojokxty5mbd.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagram-1
 export AWS_BUCKET=arn:aws:s3:::mohab-udagram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost
-export AWS_ACCESS_KEY_ID = AKIA4J3FLUA7IXAHNAFM
-export AWS_SECRET_ACCESS_KEY = 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
-export PORT = 8080
+export URL=http://udagram-1.cojokxty5mbd.us-east-1.rds.amazonaws.com
+export AWS_ACCESS_KEY_ID=AKIA4J3FLUA7KNPIV3GU
+export AWS_SECRET_ACCESS_KEY=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
+export PORT =8080
 export DB_PORT=5432
-export RDS_DIALECT = "postgres"
+export RDS_DIALECT="postgres"
